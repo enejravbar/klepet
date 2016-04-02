2e9041e2ef61da7db0dee6112d59c43363f9ac07
Enej Ravbar <enej.ravbar@siol.net>
2016-04-02 12:19:31 +0000

    popravek youtube linkov

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -5,44 +5,86 @@ function divElementEnostavniTekst(sporocilo) {
     
       textZaPoslat = obdelajBesediloSporocila(sporocilo);
       console.log("Pred modifikacijo:"+textZaPoslat);
-      textZaPoslat = textZaPoslat.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(new RegExp('&lt;img', 'gi'), '<img').replace(new RegExp('png\" /&gt;', 'gi'), 'png\" />').replace(new RegExp('jpg\" /&gt;', 'gi'), 'jpg\" />').replace(new RegExp('gif\" /&gt;', 'gi'), 'gif\" />')
-      .replace(new RegExp('&lt;iframe', 'gi'), '<iframe').replace(new RegExp('&gt;&lt;\/iframe&gt;', 'gi'), '></iframe>');
-      ;
+      textZaPoslat = textZaPoslat.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(new RegExp('&lt;img', 'gi'), '<img').replace(new RegExp('png\' /&gt;', 'gi'), 'png\' />').replace(new RegExp('jpg\' /&gt;', 'gi'), 'jpg\' />').replace(new RegExp('gif\' /&gt;', 'gi'), 'gif\' />').replace(new RegExp('&lt;iframe', 'gi'), '<iframe').replace(new RegExp('&gt;&lt;\/iframe&gt;', 'gi'), '></iframe>');
+      
       console.log("Po modifikacijo:"+textZaPoslat);
       return  $('<div style="font-weight: bold;"></div>').html(textZaPoslat);
    
   }else {
+    	    sporocilo=sporocilo.replace(/\"/g , "\'");
           return $('<div style="font-weight: bold;"></div>').text(sporocilo);
     }
     
   
 }
 
-function obdelajBesediloSporocila(sporocilo){
+function poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG){      // ce je -1 pomeni da v sporocilu ni URL za slike
+  var min=Number.MAX_VALUE; 
+  if(pozPNG==-1 && pozJPG==-1 && pozGIF==-1 ){
+    return -1;
+  }
+  if(pozGIF<min && pozGIF!=-1 ){
+    min=pozGIF;
+  }
+  if(pozJPG<min && pozJPG!=-1 ){
+    min=pozJPG;
+  }
+  if(pozPNG<min && pozPNG!=-1 ){
+    min=pozPNG;
+  }
+  return min;
+  
+}
+
+function poisciMinPozHTTP(pozHTTP, pozHTTPS){      // ce je -1 pomeni da v sporocilu ni URL za slike
+  var min=Number.MAX_VALUE; 
+  if(pozHTTP==-1 && pozHTTPS==-1){   //ce je -1 pomeni da v sporocilu ni VEC URL-JEV
+    return Number.MAX_VALUE;
+  }
+  if(pozHTTP<min && pozHTTP!=-1 ){
+    min=pozHTTP;
+  }
+  if(pozHTTPS<min && pozHTTPS!=-1 ){
+    min=pozHTTPS;
+  }
+  
+  return min;
+  
+}
+function obdelajBesediloSporocila(sporocilo1){
 		var text="";
 		var kontrola=0;
 		var poz=0;
 		var pozicijaPNG=0;
+		var sporocilo=sporocilo1;
 		
+		var pozPNG=0;
+	  var pozJPG=0;
+		var pozGIF=0;
+		var pozHTTP=0;
+		var pozHTTPS=0;
+		
+	//	sporocilo=sporocilo.replace(/\"/g , "\'");
+	
+		console.log(sporocilo);
 		for(var i=0; i<sporocilo.length; i++){
 			if(poz>=sporocilo.length){break;}
-        
+			
 		    if(sporocilo.charAt(poz)=='h' || sporocilo.charAt(poz)=='.'){
-		     
-		      if( sporocilo.length>=poz+47 ){
+
+		      if( sporocilo.length>=poz+47 ){   // naredi smejkota
 		        pozicijaPNG=0;
 			      if((sporocilo.substring(poz,poz+47)==('http://sandbox.lavbic.net/teaching/OIS/gradivo/') )){ 
 			        //text=text+'<img src="'+sporocilo.charAt(poz);
 			        pozicijaPNG=sporocilo.indexOf(".png",poz);
-			        console.log("POzicija .png je " +pozicijaPNG);
-			        text=text+'<img src="'+sporocilo.substring(poz,pozicijaPNG+3)+sporocilo.charAt(pozicijaPNG+3);
+			        console.log("Pozicija .png je " +pozicijaPNG);
+			        text=text+'<img src='+"'"+sporocilo.substring(poz,pozicijaPNG+3)+sporocilo.charAt(pozicijaPNG+3);
 			        
 			        poz=pozicijaPNG+4;
 			        continue;
 			      }
 			  }
-		     
-		     //youtube iframe 
+			   //youtube iframe 
 		     
 		     if( sporocilo.length>=poz+32 ){
 		       var pozicijaID=0;
@@ -55,26 +97,68 @@ function obdelajBesediloSporocila(sporocilo){
 			        continue;
 			      }
 			  }
-		     
+		    
 		     if( sporocilo.length>=poz+8 ){
+		       
 			      if((sporocilo.substring(poz,poz+8)==("https://") )){ //preverimo ce je v sporocilu https://
-			        text=text+'<img style="display:block; width:200px; margin-left:20px;" src="'+sporocilo.charAt(poz);
-			        poz++;
-			        continue;
+			        pozPNG=sporocilo.indexOf(".png",poz+1);
+		          pozJPG=sporocilo.indexOf(".jpg",poz+1);
+		          pozGIF=sporocilo.indexOf(".gif",poz+1);
+		          pozHTTP=sporocilo.indexOf("http://",poz+1);
+		          pozHTTPS=sporocilo.indexOf("https://",poz+1);
+		          console.log("pozPNG="+pozPNG+" pozJPG=" +pozJPG+ " pozGIF=" +pozGIF );
+		          console.log("pozHTTP="+pozHTTP+" pozHTTPS=" +pozHTTPS);
+		          console.log("Minimalna pozicija koncnice slike: " + poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG) + "    Minimalna pozicija http://: " +  poisciMinPozHTTP(pozHTTP, pozHTTPS));
+			        
+			        if(poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG)==-1){  // ce ni slikovnih url-jev obravnavaj sporocilo kot text
+			          text=text+sporocilo.charAt(poz);
+			          poz++;
+			          continue;
+			        }
+			        if(poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG) < poisciMinPozHTTP(pozHTTP, pozHTTPS)){ 
+			          text=text+'<img style="display:block; width:200px; margin-left:20px;" src=\''+sporocilo.charAt(poz);
+			          poz++;
+			          continue;
+			        }
+			        
+			        
+			        
 			      }
 			 }
 
-			 if(sporocilo.length>=poz+7){
+			if(sporocilo.length>=poz+7){
+			      
 			      if( (sporocilo.substring(poz,poz+7)==("http://") )){  // preverimo, ce je v sporocilu http://
-			        text=text+'<img style="display:block; width:200px; margin-left:20px;" src="'+sporocilo.charAt(poz);
-			        poz++;
-			        continue;	
+			       
+			        pozPNG=sporocilo.indexOf(".png",poz+1);
+		          pozJPG=sporocilo.indexOf(".jpg",poz+1);
+		          pozGIF=sporocilo.indexOf(".gif",poz+1);
+		          pozHTTP=sporocilo.indexOf("http://",poz+1);
+		          pozHTTPS=sporocilo.indexOf("https://",poz+1);
+		          console.log("pozPNG="+pozPNG+" pozJPG=" +pozJPG+ " pozGIF=" +pozGIF );
+		          console.log("pozHTTP="+pozHTTP+" pozHTTPS=" +pozHTTPS);
+		          console.log("Minimalna pozicija koncnice slike: " + poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG) + "    Minimalna pozicija http://: " +  poisciMinPozHTTP(pozHTTP, pozHTTPS));
+			        
+			        if(poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG)==-1){  // ce ni slikovnih url-jev obravnavaj sporocilo kot text
+			          text=text+sporocilo.charAt(poz);
+			          poz++;
+			          continue;
+			        }
+			        if(poisciMinPozKoncnice(pozGIF, pozJPG, pozPNG) < poisciMinPozHTTP(pozHTTP, pozHTTPS)){ 
+			          text=text+'<img style="display:block; width:200px; margin-left:20px;" src=\''+sporocilo.charAt(poz);
+			          poz++;
+			          continue;
+			        }
+			     
 			      }
 			 }
 		     
+		     
+		     
+		     
 		     if((sporocilo.length)>poz+3){    
 			      if((  (sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3))==(".jpg"))){ //preverimo ce je v sporocilu .gif
-			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'" />';
+			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'\' />';
 			        poz=poz+4;
 			         if(poz>=sporocilo.length){break;}
 			        continue;
@@ -83,7 +167,7 @@ function obdelajBesediloSporocila(sporocilo){
 
 			  if((sporocilo.length>poz+3)){
 			      if( ((sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3))==(".png") )){ 
-			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'" />';
+			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'\' />';
 			        poz=poz+4;
 			         if(poz>sporocilo.length){break;}
 			        continue;
@@ -94,7 +178,7 @@ function obdelajBesediloSporocila(sporocilo){
 
 			  if((sporocilo.length>poz+3)){
 			      if(( (sporocilo.substring(poz,poz+3)+ sporocilo.charAt(poz+3) )==(".gif") && (sporocilo.length)>=poz+3)){ //preverimo ce je v sporocilu .jpg
-			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'" />';
+			        text=text+sporocilo.substring(poz,poz+3)+sporocilo.charAt(poz+3)+'\' />';
 			        poz=poz+4;
 			        if(poz>sporocilo.length){break;}
 			        continue;
@@ -112,9 +196,10 @@ function obdelajBesediloSporocila(sporocilo){
 		  return text;
 
 	}
+
 	
 function divElementHtmlTekst(sporocilo) {
-  return $('<div></div>').html('<i>' + sporocilo + '</i>');
+  return divElementEnostavniTekst(sporocilo);
 }
 
 function procesirajVnosUporabnika(klepetApp, socket) {       //POSLJI SPOROCILO 
@@ -124,7 +209,9 @@ function procesirajVnosUporabnika(klepetApp, socket) {       //POSLJI SPOROCILO
 
   if (sporocilo.charAt(0) == '/') {
     sistemskoSporocilo = klepetApp.procesirajUkaz(sporocilo);
+
     if (sistemskoSporocilo) {
+      console.log( sistemskoSporocilo);
       $('#sporocila').append(divElementHtmlTekst(sistemskoSporocilo));
     }
   } else {
@@ -206,7 +293,21 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+
   });
+  
+  socket.on('dregljaj', function(){
+    $('#vsebina').jrumble();
+    $('#vsebina').trigger('startRumble');
+    setTimeout(function(){
+      $('#vsebina').trigger('stopRumble');
+    },1500);
+  });
+
+
+
+
 
   setInterval(function() {
     socket.emit('kanali');
@@ -220,9 +321,7 @@ $(document).ready(function() {
     return false;
   });
   
-  
-});
-
+ });
 function dodajSmeske(vhodnoBesedilo) {
   var preslikovalnaTabela = {
     ";)": "wink.png",
@@ -234,7 +333,7 @@ function dodajSmeske(vhodnoBesedilo) {
   for (var smesko in preslikovalnaTabela) {
     vhodnoBesedilo = vhodnoBesedilo.replace(smesko,
       "http://sandbox.lavbic.net/teaching/OIS/gradivo/" +
-      preslikovalnaTabela[smesko]+'" />');
+      preslikovalnaTabela[smesko]+"' />");
   }
   return vhodnoBesedilo;
 }
